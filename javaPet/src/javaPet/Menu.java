5e2e1dc12024cae1572a29e7a623b2de96e4b51d
RafaelAstora <rafad2106@gmail.com>
2023-02-24 10:42:03 -0300

    Menu bonitinho

--- javaPet/src/javaPet/Menu.java
+++ javaPet/src/javaPet/Menu.java
@@ -7,58 +7,64 @@ import javaPet.model.Animal;
 import javaPet.model.Cachorro;
 import javaPet.model.Gato;
 
+public class Menu {
 
-public class Menu  {
+	static int id;
 
-	static int id; 
-	
 	public static void main(String[] args) {
-   
-		ArrayList<Animal> animais = new ArrayList<Animal> ();
-		
+
+		ArrayList<Animal> animais = new ArrayList<Animal>();
+
 		Scanner leia = new Scanner(System.in);
-		
+
 		int opcao = 1;
 		int tipo;
 		double idade, peso;
 		String nome, raca, sexo, porte;
 		boolean aptoDoacao;
-		boolean felv,fiv;
-		
-		//Animal a1 = new Animal("Mel", 1, "SRD", 6, 10, "Fêmea", true);
-		//a1.visualizar();
-		
+		boolean felv, fiv;
+
+		// Animal a1 = new Animal("Mel", 1, "SRD", 6, 10, "Fêmea", true);
+		// a1.visualizar();
+
 		Cachorro c1 = new Cachorro("Gibson", 2, "Poodle", 8, 7, "Macho", true, "Pequeno");
-		c1.visualizar();
-		
+		// c1.visualizar();
+
 		Gato g1 = new Gato("Gibson", 3, "Poodle", 8, 7, "Macho", true, true, false);
-		g1.visualizar();
-		
+		// g1.visualizar();
+
+		System.out.println("                                                     ,-.___,-.   \r\n"
+				+ "                                                     \\_/_ _\\_/\r\n"
+				+ "            /\\_/\\                                      )O_O(\r\n"
+				+ "           ( o.o )                                    { (_) }\r\n"
+				+ "            > ^ <                                      `-^-' ");
+
 		do {
 			System.out.println("         *******************************************************");
-            System.out.println("         *                                                     *");
-            System.out.println("         *                 INSTITUTO JAVA PET                  *");
-            System.out.println("         *                                                     *");
-            System.out.println("         *******************************************************");
-            System.out.println("         *        1 - Cadastrar novo animal                    *");
-            System.out.println("         *        2 - Listar todos animais                     *");
-            System.out.println("         *        3 - Buscar animal por ID                     *");
-            System.out.println("         *        4 - Atualizar Dados do Animal                *");
-            System.out.println("         *        5 - Apagar Cadastro do Animal                *");
-            System.out.println("         *        6 - Estoque                                  *");
-            System.out.println("         *        0 - Sair                                     *");
-            System.out.println("         *******************************************************\n\n");
+			System.out.println("         *                                                     *");
+			System.out.println("         *                                                     *");
+			System.out.println("         *                 INSTITUTO JAVA PET                  *");
+			System.out.println("         *                                                     *");
+			System.out.println("         *                                                     *");
+			System.out.println("         *******************************************************");
+			System.out.println("         *        1 - Cadastrar novo animal                    *");
+			System.out.println("         *        2 - Listar todos animais                     *");
+			System.out.println("         *        3 - Buscar animal por ID                     *");
+			System.out.println("         *        4 - Atualizar Dados do Animal                *");
+			System.out.println("         *        5 - Apagar Cadastro do Animal                *");
+			System.out.println("         *        6 - Estoque                                  *");
+			System.out.println("         *        0 - Sair                                     *");
+			System.out.println("         *******************************************************\n\n");
 			System.out.println("Informe a opção desejada:");
 			opcao = leia.nextInt();
-			
-			if (opcao == 0) { 
+
+			if (opcao == 0) {
 				System.out.println("\nPrograma Finalizado - Instituto Java PET");
 				leia.close();
 				System.exit(0);
 			}
-				
-			
-			switch(opcao) {
+
+			switch (opcao) {
 			case 1:
 				System.out.println("Cadastrar novo animal:");
 				System.out.println(" ");
@@ -66,15 +72,15 @@ public class Menu  {
 				nome = leia.nextLine();
 				System.out.println("Insira a Raça:   ");
 				raca = leia.nextLine();
-                System.out.println("Insira a idade do animal:   ");
-                idade = leia.nextDouble();
-                leia.skip("\\R?");
-                System.out.println("Insira o sexo do animal:    ");
+				System.out.println("Insira a idade do animal:   ");
+				idade = leia.nextDouble();
+				leia.skip("\\R?");
+				System.out.println("Insira o sexo do animal:    ");
 				sexo = leia.nextLine();
 				System.out.println("Insira o peso do animal:    ");
 				peso = leia.nextDouble();
 				aptoDoacao = false;
-				
+
 				System.out.println("Digite 1 = Gato ou 2 = Cachorro");
 				tipo = leia.nextInt();
 				switch (tipo) {
@@ -83,51 +89,49 @@ public class Menu  {
 					felv = leia.nextBoolean();
 					System.out.println("Ele é positivo para fiv?");
 					fiv = leia.nextBoolean();
-						
-				case 2: 
+
+				case 2:
 					System.out.println("Qual o porte do animal?");
 					porte = leia.nextLine();
-			
+
 				}
-				
-				//animais.add();
-				
+
+				// animais.add();
+
 				break;
-				
+
 			case 2:
 				System.out.println("Listar todos animais:");
-				
+
 				break;
-			
+
 			case 3:
 				System.out.println("Buscar animal por ID:");
-				
+
 				break;
-				
+
 			case 4:
 				System.out.println("Atualizar dados do animal:");
-				
+
 				break;
-				
+
 			case 5:
 				System.out.println("Apagar cadastro do animal:");
 				break;
-				
+
 			case 6:
 				System.out.println("Estoque:");
-				
+
 				break;
-				
+
 			default:
 				System.out.println("Opção Inválida!");
 				break;
 			}
-			
-		} while(opcao != 0);
-		
-		//leia.close();
+
+		} while (opcao != 0);
+
+		// leia.close();
 	}
-		
+
 }
-
-
